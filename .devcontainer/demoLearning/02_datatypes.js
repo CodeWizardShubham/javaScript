af2d141bc77e8d8c75120ac259e7906a0de674b3
CodeWizardShubham <148583239+CodeWizardShubham@users.noreply.github.com>
2023-10-22 19:44:15 +0000

    variables 02

---
+++ .devcontainer/demoLearning/02_datatypes.js
@@ -0,0 +1,23 @@
+"user strict"; // treat all JS code as new version
+// alert(3+3 ) --> we are using node js not web browser
+
+console.log(1234) 
+console.log("deepak")
+
+let name = "shubham"
+account = 123;
+
+console.log( typeof undefined);
+
+/*
+date types :
+
+integer
+bigint
+string
+boolean 
+null --> standalone value 
+undefined --> undefined
+symbol --> unique 
+
+*/
