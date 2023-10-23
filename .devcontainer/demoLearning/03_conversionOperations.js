e07848d283f0075d5d2a0d4603e5f7881d28ed05
CodeWizardShubham <148583239+CodeWizardShubham@users.noreply.github.com>
2023-10-23 18:06:17 +0000

    commit

---
+++ .devcontainer/demoLearning/03_conversionOperations.js
@@ -0,0 +1,10 @@
+let emplyeeid = "22345343"
+
+
+console.log( typeof vauleInNumber);
+console.log( typeof(emplyeeid));
+
+
+let vauleInNumber = Number(emplyeeid)
+console.log(typeof vauleInNumber);
+console.log(vauleInNumber);
