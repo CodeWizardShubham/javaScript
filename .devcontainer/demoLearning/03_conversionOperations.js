b69208113450462af5b2c472d7dd5e6e33434a0f
CodeWizardShubham <148583239+CodeWizardShubham@users.noreply.github.com>
2023-10-25 18:57:13 +0000

    onversion of date type

--- .devcontainer/demoLearning/03_conversionOperations.js
+++ .devcontainer/demoLearning/03_conversionOperations.js
@@ -1,10 +1,28 @@
-let emplyeeid = "22345343"
+let emplyeeid = "22345343dwd"
 
 
-console.log( typeof vauleInNumber);
-console.log( typeof(emplyeeid));
+// console.log( typeof valueInNumber);
+// console.log( typeof(emplyeeid));
 
 
-let vauleInNumber = Number(emplyeeid)
-console.log(typeof vauleInNumber);
-console.log(vauleInNumber);
+let valueInNumber = Number(emplyeeid) //Making the conversion
+// console.log(typeof vauleInNumber);
+// console.log(valueInNumber);
+
+// "33" => easily convert
+//"33abc" => NaN (Nota Number)
+//"true" => 1 ; "flase" => 0
+
+let isLogedIn = ""
+
+let BooleanLogedIn = Boolean(isLogedIn)
+// console.log(BooleanLogedIn);
+
+// 1 => true , 0=> false
+// "" => False
+// "Shuvan" => true
+
+let someNumber = 33
+let a = String(someNumber)
+console.log(a);
+console.log(typeof a);
